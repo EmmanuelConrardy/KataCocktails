7c2beefb529ed93654e32596f9642b6819b7506a
emmanuel.conrardy <emmanuel.conrardy@acadomia.fr>
2021-07-06 19:10:52 +0200

    init

---
+++ KataCocktails.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace KataCocktails.Tests
+{
+    public class FizzBuzzTest
+    {
+        [Fact]
+        public void Rename_me()
+        {
+
+        }
+    }
+}
